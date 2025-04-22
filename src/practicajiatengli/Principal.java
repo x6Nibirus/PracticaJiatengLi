72fec3ce2875fa9285232c21cb5f5278e05b66aa
Jiateng <jiatengli7@gmail.com>
2025-04-22 18:50:03 +0200

    Se valida el dato introducido

--- src/practicajiatengli/Principal.java
+++ src/practicajiatengli/Principal.java
@@ -4,17 +4,23 @@ import java.util.Scanner;
 
 public class Principal {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Introduzca el número sobre el que hacer los cálculos");
-		
-		int numero = sc.nextInt();
-		
-		new CalculosMatematicos().calculos(numero);
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-	}
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduzca el número sobre el que hacer los cálculos");
+
+        int numero;
+        try {
+            numero = sc.nextInt();
+        } catch (Exception e) {
+            System.out.println("El dato introducido no es un n�mero");
+            return;
+        }
+
+        new CalculosMatematicos().calculos(numero);
+
+    }
 
 }
